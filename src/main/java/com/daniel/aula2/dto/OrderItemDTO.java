4cdd558243c40750964b8e61b012c473b79089df
Battisti-Daniel <danielbattisti@outlook.com.br>
2026-01-13 17:37:10 -0300

    Add urlImg to Order

--- src/main/java/com/daniel/aula2/dto/OrderItemDTO.java
+++ src/main/java/com/daniel/aula2/dto/OrderItemDTO.java
@@ -8,12 +8,14 @@ public class OrderItemDTO {
     private String name;
     private Double price;
     private Integer quantity;
+    private String imgUrl;
 
-    public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
+    public OrderItemDTO(Long productId, String name, Double price, Integer quantity, String imgUrl) {
         this.productId = productId;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.imgUrl = imgUrl;
     }
 
     public OrderItemDTO(OrderItem entity) {
@@ -21,6 +23,7 @@ public class OrderItemDTO {
         this.name = entity.getProduct().getName();
         this.price = entity.getPrice();
         this.quantity = entity.getQuantity();
+        this.imgUrl = entity.getProduct().getImgUrl();
     }
 
     public OrderItemDTO() {
@@ -44,5 +47,9 @@ public class OrderItemDTO {
 
     public Double getSubTotal(){
         return this.quantity * this.price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
     }
 }

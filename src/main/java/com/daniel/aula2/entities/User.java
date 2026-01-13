2c563d40ee2bd685bd8e0c8a518f828af778e011
Battisti-Daniel <danielbattisti@outlook.com.br>
2026-01-13 11:31:44 -0300

    Domain model User Role, seed

--- src/main/java/com/daniel/aula2/entities/User.java
+++ src/main/java/com/daniel/aula2/entities/User.java
@@ -2,15 +2,11 @@ package com.daniel.aula2.entities;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_user")
@@ -30,13 +26,32 @@ public class User {
 
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
-    
-    
+
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public boolean hasRole(String roleName) {
+        for (Role role : roles) {
+            if (role.getAuthority().equals(roleName)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public User() {
     }
-
-
 
     public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
         this.id = id;
